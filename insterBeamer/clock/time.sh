044e21d4992e2b6c6f35a209b3416c2a1e8e2ad0
David Laubenstein <david.laubenstein@posteo.de>
2017-01-08 20:07:02 +0100

    change paths

--- insterBeamer/clock/time.sh
+++ insterBeamer/clock/time.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $(date +%H):$(date +%M) Uhr > /opt/info-beamer/info-beamer-pi/insterBeamer/clock/time.txt
+echo $(date +%H):$(date +%M) Uhr > /opt/info-beamer/insterBeamer/insterBeamer/clock/time.txt
--- insterBeamer/weather/day.sh
+++ insterBeamer/weather/day.sh
@@ -1,7 +1,7 @@
-echo $(date +%a", "%e"."%m"."%y)": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day0.txt
-echo $(date +%a", "%e"."%m"."%y --date="+1 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day1.txt
-echo $(date +%a", "%e"."%m"."%y --date="+2 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day2.txt
-echo $(date +%a", "%e"."%m"."%y --date="+3 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day3.txt
-echo $(date +%a", "%e"."%m"."%y --date="+4 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day4.txt
-echo $(date +%a", "%e"."%m"."%y --date="+5 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day5.txt
-echo $(date +%a", "%e"."%m"."%y --date="+6 day")": " > /opt/info-beamer/info-beamer-pi/insterBeamer/weather/day6.txt
+echo $(date +%a", "%e"."%m"."%y)": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day0.txt
+echo $(date +%a", "%e"."%m"."%y --date="+1 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day1.txt
+echo $(date +%a", "%e"."%m"."%y --date="+2 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day2.txt
+echo $(date +%a", "%e"."%m"."%y --date="+3 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day3.txt
+echo $(date +%a", "%e"."%m"."%y --date="+4 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day4.txt
+echo $(date +%a", "%e"."%m"."%y --date="+5 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day5.txt
+echo $(date +%a", "%e"."%m"."%y --date="+6 day")": " > /opt/info-beamer/insterBeamer/insterBeamer/weather/day6.txt
